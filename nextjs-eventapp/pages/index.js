c97b0dfa6f3e1c6340ffc878909589c429366117
Deep-Ramani <deepramani8505@gmail.com>
2022-09-26 12:12:48 +0530

    Create All Event Page with Data fetching And browse all Events

--- nextjs-eventapp/pages/index.js
+++ nextjs-eventapp/pages/index.js
@@ -1,11 +1,13 @@
-import { getFeaturedEvents } from "../dummy-data"
-import EventList from "../components/events/event-list"
+import { getFeaturedEvents } from "../dummy-data";
+import EventList from "../components/events/event-list";
 export default function HomePage() {
-
-  const featuredEvents =getFeaturedEvents()
+  const featuredEvents = getFeaturedEvents();
   return (
     <div>
-      <EventList items={featuredEvents}/>
+      <header>
+        <nav></nav>
+      </header>
+      <EventList items={featuredEvents} />
     </div>
-  )
+  );
 }
