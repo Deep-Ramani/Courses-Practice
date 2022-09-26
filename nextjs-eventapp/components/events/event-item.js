cd608036d4d2f1444694c06761e414d48e36eeba
Deep-Ramani <deepramani8505@gmail.com>
2022-09-26 11:04:22 +0530

    Create Event HomePage with Data fetching

--- nextjs-eventapp/components/events/event-item.js
+++ nextjs-eventapp/components/events/event-item.js
@@ -1,32 +1,46 @@
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
+import classes from "./event-item.module.css";
+import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 const EventItem = (props) => {
+  const { title, image, date, location, id } = props;
 
-  const {title,image,date,location,id} = props;
-
-  const humanRedableDate = new Date(date).toLocaleDateString('en-US',{
-    day:'numeric',
-    month:'long',
-    year: numeric,
+  const humanRedableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
   });
 
-  
+  const formattedAddress = location.replace(",", "\n");
+  const exploreLink = `events/${id}`;
+
   return (
-    <li>
-      
-      <div>
-        <div>
-          <h2>TITLE</h2>
-          <div>
-            <time>DATE</time>
+    <li className={classes.item}>
+      <Image src={"/" + image} alt={title} width="200px" height="200px" />
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{title}</h2>
+          <div className={classes.date}>
+            <DateIcon />
+            <time>{humanRedableDate}</time>
           </div>
-          <div>
-            <address>ADDRESS</address>
+          <div className={classes.address}>
+            <address>{formattedAddress}</address>
+            <AddressIcon />
           </div>
         </div>
-        <div>
-          <Link href="/">Explore Event</Link>
+        <div className={classes.actions}>
+          <Button link={exploreLink}>
+            <span>Expolre Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
