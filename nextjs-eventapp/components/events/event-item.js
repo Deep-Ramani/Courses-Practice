41673d5ce54fef683d8dfa39f74211b69fdfcb68
Deep-Ramani <deepramani8505@gmail.com>
2022-09-26 18:51:33 +0530

    Completed Filtered Event functionality

--- nextjs-eventapp/components/events/event-item.js
+++ nextjs-eventapp/components/events/event-item.js
@@ -17,7 +17,7 @@ const EventItem = (props) => {
   });
 
   const formattedAddress = location.replace(",", "\n");
-  const exploreLink = `events/${id}`;
+  const exploreLink = `/events/${id}`;
 
   return (
     <li className={classes.item}>
--- nextjs-eventapp/pages/events/index.js
+++ nextjs-eventapp/pages/events/index.js
@@ -1,12 +1,20 @@
+import { useRouter } from "next/router";
 import React from "react";
 import EventList from "../../components/events/event-list";
-import {getAllEvents} from '../../dummy-data'
+import EventSearch from "../../components/events/event-search";
+import { getAllEvents } from "../../dummy-data";
 const AllEventPage = () => {
-
   const events = getAllEvents();
+  const router= useRouter();
+
+  function findEventHandler(year,month){
+      const fullPath = `/events/${year}/${month}`
+      router.push(fullPath)
+  }
   return (
     <div>
-      <EventList items={events}/>
+      <EventSearch onSearch={findEventHandler}/>
+      <EventList items={events} />
     </div>
   );
 };
